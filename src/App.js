a0090144aed5effcade3a8a168639750c4ebc690
MarcinBalejko <mbbm9@o2.pl>
2020-08-08 22:58:25 +0200

    :white_check_mark::construction: Created notification, popup, word and wrongLetters components, set the initial state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
 import "./App.css";
 
+const words = ["application", "programming", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
-  return <Header />;
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
+  );
 }
 
 export default App;
